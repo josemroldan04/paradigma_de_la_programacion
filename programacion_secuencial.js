21f67071a2aa651c3a83f382aa574de20fa298ff
josemroldan.04 <josemroldan.04@gmail.com>
2026-02-25 16:01:51 -0500

    Escribi el nombre y las operaciones

---
+++ programacion_secuencial.js
@@ -0,0 +1,20 @@
+let nombre = "Jose";
+let edad = 16;
+let apellido = "Roldan";
+let numero = 98;
+
+function sumar(a, b) {
+    return a + b;
+}
+
+function restar(a, b) {
+    let operacion = a - b;
+    return operacion;
+}
+
+
+console.log("El resultado de la suma es: " +sumar(numero, edad)) 
+let ResultadoResta = restar(9, 3)
+
+console.log("El resultado de la resta es: " + ResultadoResta)
+console.log("Nombre completo: " + nombre + " " + apellido + ", Edad: " + edad);
